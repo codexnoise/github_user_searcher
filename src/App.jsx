efba0725a9816815a7e853a59e69627f27451413
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-09-30 18:03:53 -0500

    add ThemeProvider component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import React from "react";
-import { Container, Typography } from "@mui/material";
+import {
+  Container,
+  Typography,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
 import Searcher from "./components/Searcher";
 import { useState, useEffect } from "react";
 import getGithubUser from "./services/users";
 import UserCard from "./containers/UserCard";
 import UserNotFound from "./components/UserNotFound";
+import { orange } from "@mui/material/colors";
 
 const App = () => {
   const [inputUser, setinputUser] = useState("octocat");
   const [userData, setUserData] = useState({});
   const [userNotFound, setUserNotFound] = useState(false);
+
+  const theme = createTheme({
+    status: {
+      danger: orange[500],
+    },
+  });
 
   const gettingUser = async (user) => {
     const userRes = await getGithubUser(user);
@@ -33,28 +45,30 @@ const App = () => {
   }, [inputUser]);
 
   return (
-    <Container
-      sx={{
-        background: "whitesmoke",
-        width: "80vw",
-        height: "500px",
-        borderRadius: "16px",
-        marginTop: "50px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Typography sx={{ fontFamily: "Courier New" }} variant="h5">
-        devsearcher
-      </Typography>
-      <Searcher inputUser={inputUser} setinputUser={setinputUser}></Searcher>
-      {userNotFound ? (
-        <UserNotFound />
-      ) : (
-        <UserCard userData={userData}></UserCard>
-      )}
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container
+        sx={{
+          background: "whitesmoke",
+          width: "80vw",
+          height: "500px",
+          borderRadius: "16px",
+          marginTop: "50px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography sx={{ fontFamily: "Courier New" }} variant="h5">
+          devsearcher
+        </Typography>
+        <Searcher inputUser={inputUser} setinputUser={setinputUser}></Searcher>
+        {userNotFound ? (
+          <UserNotFound />
+        ) : (
+          <UserCard userData={userData}></UserCard>
+        )}
+      </Container>
+    </ThemeProvider>
   );
 };
 
