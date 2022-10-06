ea9d62f707d5c11e23965776263cf23b025a16a9
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-10-05 23:54:56 -0500

    add styles to Lenguages component

--- src/components/Lenguages.jsx
+++ src/components/Lenguages.jsx
@@ -1,14 +1,14 @@
+import { Typography } from "@mui/material";
 import React from "react";
 
 const Lenguages = (props) => {
   const { userLanguages } = props;
+
+  const lenguages = userLanguages.join(", ");
   return (
-    <div>
-      Lenguage(s):
-      {userLanguages.map((lenguage) => (
-        <li key={lenguage}>{lenguage}</li>
-      ))}
-    </div>
+    <Typography sx={{ fontFamily: "Courier New" }} variant="h6">
+      {` ${lenguages}`}
+    </Typography>
   );
 };
 
